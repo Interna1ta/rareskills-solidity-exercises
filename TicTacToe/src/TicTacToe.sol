e8386495a2167b4c3e2b4a68c8f7297028b117f7
Pere <pere@MacBook-Pro-de-Pere.local>
2024-03-19 16:17:25 -0300

    solve tictactoe

--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -12,5 +12,22 @@ contract TicTacToe {
 
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
         // your code here
+        for (uint8 i = 0; i < board.length; i++) {
+            for (uint8 j = 0; j < board[0].length; j++) {
+                if (board[i][0] == 1 && board[i][1] == 1 && board[i][2] == 1) {
+                    return true;
+                }
+                if (board[0][j] == 1 && board[1][j] == 1 && board[2][j] == 1) {
+                    return true;
+                }
+                if (board[0][0] == 1 && board[1][1] == 1 && board[2][2] == 1) {
+                    return true;
+                }
+                if (board[0][2] == 1 && board[1][1] == 1 && board[2][0] == 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
