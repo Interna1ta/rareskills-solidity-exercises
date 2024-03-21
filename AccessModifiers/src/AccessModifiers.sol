a242fa0ffd36e3bb67b30d78915361d1de3cc3cd
Pere <pere@MacBook-Pro-de-Pere.local>
2024-03-21 17:28:16 -0300

    solve AccessModifiers

--- AccessModifiers/src/AccessModifiers.sol
+++ AccessModifiers/src/AccessModifiers.sol
@@ -3,21 +3,33 @@ pragma solidity ^0.8.13;
 
 // You may modify this contract
 contract Parent {
-    uint256 private _value;
+    uint256 private value;
+
+    constructor(uint256 _value) {
+        value = _value;
+    }
+
+    function setValue(uint256 _newValue) public virtual {
+        value = _newValue;
+    }
+
+    function getValue() public view virtual returns (uint256) {
+        return value;
+    }
 }
 
-contract Child is Parent {
+contract Child is Parent(10) {
     /*
         This exercise assumes you understand how access modifiers works.
         1. `_value` variable is private and can only be accessed by `Parent` contract. Make the variable accessible
            to `Parent` and `Child` contract ONLY.
     */
 
-    function setValue(uint256 newValue) public {
-        _value = newValue;
+    function setValue(uint256 newValue) public override {
+        super.setValue(newValue);
     }
 
-    function getValue() public view returns (uint256) {
-        return _value;
+    function getValue() public view override returns (uint256) {
+        return super.getValue();
     }
 }
